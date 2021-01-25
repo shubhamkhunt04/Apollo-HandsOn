73a4b5a346fbb1604c4d7446b532c0564a1f0294
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-01-25 15:50:04 +0530

    apollo-handson

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import ExchangeRates from "./Component/ExchangeRates";
+import User from "./Component/User";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ExchangeRates /> */}
+      <User />
+    </div>
+  );
+}
+
+export default App;
